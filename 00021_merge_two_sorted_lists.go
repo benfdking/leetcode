57eece5a44c6ce22d294a35da3e57904210b154b
Ben <9087625+benfdking@users.noreply.github.com>
2022-07-10 19:16:07 +0200

    20 21 challenges (#3)
    * temp
    
    * Finish 20 valid parenthesis
    
    * Before transforming it to be recursive
    
    * Speeding up slightly

---
+++ 00021_merge_two_sorted_lists.go
@@ -0,0 +1,23 @@
+package leetcode
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	if list1 == nil && list2 == nil {
+		return nil
+	}
+	if list1 == nil {
+		return list2
+	}
+	if list2 == nil {
+		return list1
+	}
+	if list1.Val < list2.Val {
+		return &ListNode{
+			Val:  list1.Val,
+			Next: mergeTwoLists(list1.Next, list2),
+		}
+	}
+	return &ListNode{
+		Val:  list2.Val,
+		Next: mergeTwoLists(list1, list2.Next),
+	}
+}
