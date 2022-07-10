84f36dcb6ed085ee405da3976813085078964b03
Ben King <9087625+benfdking@users.noreply.github.com>
2022-07-10 13:10:25 +0200

    Before transforming it to be recursive

--- 00021_merge_two_sorted_lists.go
+++ 00021_merge_two_sorted_lists.go
@@ -1,47 +1,29 @@
 package leetcode
 
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
-//func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
-//	l1 := list1
-//	l2 := list2
-//	out := &ListNode{}
-//	for {
-//
-//		if l1.Next == nil {
-//			return
-//		}
-//	}
-//}
-
-//You are given the heads of two sorted linked lists list1 and list2.
-//
-//Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
-//
-//Return the head of the merged linked list.
-//
-//Example 1:
-//
-//Input: list1 = [1,2,4], list2 = [1,3,4]
-//Output: [1,1,2,3,4,4]
-//
-//Example 2:
-//
-//Input: list1 = [], list2 = []
-//Output: []
-//
-//Example 3:
-//
-//Input: list1 = [], list2 = [0]
-//Output: [0]
-//
-//Constraints:
-//
-//The number of nodes in both lists is in the range [0, 50].
-//-100 <= Node.val <= 100
-//Both list1 and list2 are sorted in non-decreasing order.
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	if list1 == nil && list2 == nil {
+		return nil
+	}
+	if list1 == nil {
+		return &ListNode{
+			Val:  list2.Val,
+			Next: list2.Next,
+		}
+	}
+	if list2 == nil {
+		return &ListNode{
+			Val:  list1.Val,
+			Next: list1.Next,
+		}
+	}
+	if list1.Val < list2.Val {
+		return &ListNode{
+			Val:  list1.Val,
+			Next: mergeTwoLists(list1.Next, list2),
+		}
+	}
+	return &ListNode{
+		Val:  list2.Val,
+		Next: mergeTwoLists(list1, list2.Next),
+	}
+}
