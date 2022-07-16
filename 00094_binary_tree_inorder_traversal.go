24b3a6eaf18bbd891ae1655853427fd9e98912cd
Ben King <9087625+benfdking@users.noreply.github.com>
2022-07-16 15:50:02 +0100

    Finish task 94
    Type: Feat

---
+++ 00094_binary_tree_inorder_traversal.go
@@ -0,0 +1,20 @@
+package leetcode
+
+func inorderTraversal(root *TreeNode) []int {
+	if root == nil {
+		return []int{}
+	}
+	return append(
+		append(
+			inorderTraversal(root.Left),
+			root.Val,
+		),
+		inorderTraversal(root.Right)...,
+	)
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
