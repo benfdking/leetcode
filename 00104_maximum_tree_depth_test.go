62006e90929f46ac44389a4bb83f57bca3f206c1
Ben King <9087625+benfdking@users.noreply.github.com>
2022-07-17 12:18:56 +0100

    Finish 104

---
+++ 00104_maximum_tree_depth_test.go
@@ -0,0 +1,46 @@
+package leetcode
+
+import "testing"
+
+func Test_maxDepth(t *testing.T) {
+	tests := []struct {
+		name string
+		root *TreeNode
+		want int
+	}{
+		{
+			name: "base 0",
+			root: nil,
+			want: 0,
+		},
+		{
+			name: "base 1",
+			root: &TreeNode{
+				Val: 1,
+			},
+			want: 1,
+		},
+		{
+			name: "multiple level example",
+			root: &TreeNode{
+				Val: 3,
+				Left: &TreeNode{
+					Val: 9,
+				},
+				Right: &TreeNode{
+					Val:   20,
+					Left:  &TreeNode{Val: 15},
+					Right: &TreeNode{Val: 7},
+				},
+			},
+			want: 3,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxDepth(tt.root); got != tt.want {
+				t.Errorf("maxDepth() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
