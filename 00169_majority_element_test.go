fd0a205ef751a2c88d9ac923444642984e04ff0a
Ben King <9087625+benfdking@users.noreply.github.com>
2022-07-23 01:11:35 +0200

    Finish 169

---
+++ 00169_majority_element_test.go
@@ -0,0 +1,29 @@
+package leetcode
+
+import "testing"
+
+func Test_majorityElement(t *testing.T) {
+	tests := []struct {
+		name string
+		nums []int
+		want int
+	}{
+		{
+			name: "base",
+			nums: []int{3},
+			want: 3,
+		},
+		{
+			name: "base with two elements",
+			nums: []int{3, 1, 3},
+			want: 3,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := majorityElement(tt.nums); got != tt.want {
+				t.Errorf("majorityElement() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
