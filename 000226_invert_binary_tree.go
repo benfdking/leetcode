01166f8cec4bfa2ff3f953643ba89dfee11492e1
Ben King <9087625+benfdking@users.noreply.github.com>
2022-07-23 00:46:29 +0200

    Finish 226

---
+++ 000226_invert_binary_tree.go
@@ -0,0 +1,12 @@
+package leetcode
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	return &TreeNode{
+		Val:   root.Val,
+		Left:  invertTree(root.Right),
+		Right: invertTree(root.Left),
+	}
+}
