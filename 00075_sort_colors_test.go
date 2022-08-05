8da2ff24ecc3b3a52293a0e9a714f15141572c1a
Ben King <9087625+benfdking@users.noreply.github.com>
2022-08-05 20:38:40 +0100

    Finish 75 sort colors

---
+++ 00075_sort_colors_test.go
@@ -0,0 +1,32 @@
+package leetcode
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_sortColors(t *testing.T) {
+	tests := []struct {
+		name string
+		nums []int
+		want []int
+	}{
+		{
+			name: "empty",
+			nums: []int{},
+			want: []int{},
+		},
+		{
+			name: "base with all",
+			nums: []int{2, 1, 0},
+			want: []int{0, 1, 2},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if sortColors(tt.nums); !reflect.DeepEqual(tt.nums, tt.want) {
+				t.Errorf("climbStairs() = %v, want %v", tt.nums, tt.want)
+			}
+		})
+	}
+}
